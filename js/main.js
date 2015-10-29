8a554a502935e31d999fcb11b5053d69f8a6c477
Clayton Jones <clayton.jones93@googlemail.com>
2015-10-29 16:03:27 +0000

    Added different calendar view options

--- js/main.js
+++ js/main.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
     // page is now ready, initialize the calendar...
 
     $('#calendar').fullCalendar({
-        // put your options and callbacks here
+       
+        header: {
+	        left:   'title',
+		    center: 'agendaDay,agendaWeek,month',
+		    right:  'today prev,next'
+		}
+
     })
 
 });
