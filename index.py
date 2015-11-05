a9ca4b0eb6b5c595c24219356f0bffd7820fcd44
Thomas <tl5g13@soton.ac.uk>
2015-11-05 22:38:26 +0000

    I PULLED BEFORE I PUSHED MOFO - Fixed the modal selecting

--- index.py
+++ index.py
@@ -213,6 +213,20 @@ TEST_HTML = """<html class="no-js" lang="">
                                         </ul>
                                     </div>
                                   </div><!-- /input-group -->
+                                  <script>
+                                  $( document.body ).on( 'click', '.dropdown-menu li', function( event ) {
+
+                                     var $target = $( event.currentTarget );
+
+                                     $target.closest( '.btn-group' )
+                                        .find( '[data-bind="label"]' ).text( $target.text() )
+                                           .end()
+                                        .children( '.dropdown-toggle' ).dropdown( 'toggle' );
+
+                                     return false;
+
+                                  });
+                                  </script>
                                   <br>
                                     <!-- date picker -->
                                         <input type="text" data-provide="datepicker" placeholder="Deadline Date" class="form-control" aria-label="...">
