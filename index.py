5367fa0c93e19d8eba57c5293797d0181419fe21
Ellie Hamilton <ellie@buckleconsulting.com>
2015-11-05 15:43:31 +0000

    started adding login.php

--- index.py
+++ index.py
@@ -178,7 +178,7 @@ SPLASH_HTML = """<!DOCTYPE html>
                 <h1 style="color:black">Sort My Life Out</h1>
                 <hr>
                 <p style="color:black">Blah blah bio Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-                <a href="#about" class="btn btn-primary btn-xl page-scroll">Start Organising</a>
+                <a href="php/login.php" class="btn btn-primary btn-xl page-scroll">Start Organising</a>
             </div>
         </div>
     </header>
