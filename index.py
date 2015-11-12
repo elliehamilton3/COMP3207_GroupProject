a5f24cda5ad65bb2820da0687d3a566f154e7519
Clayton Jones <clayton.jones93@googlemail.com>
2015-11-12 15:42:19 +0000

    Feed now runs out of a python function
    Importred json

--- index.py
+++ index.py
@@ -9,6 +9,7 @@ import logging
 import traceback
 import webapp2
 import datetime
+import json
 
 TEST_HTML = """<html class="no-js" lang="">
     <head>
@@ -490,11 +491,76 @@ class Test(webapp2.RequestHandler):
 			self.response.write(SPLASH_HTML)'''
 
 
+# ///////////////////////////////////
+
+# JSON Feed
+
+def jsonfeed():
+
+    json_list = []
+
+    # for entry in entries:
+    idd = 12
+    title = 'My event'
+    start = '2015-11-05T09:20:22+00:00'
+    end = '2015-11-05T13:20:22+00:00'
+
+    json_entry = {'id':idd, 'start':start, 'end':end, 'title': title}
+
+    # print json_entry
+
+    json_list.append(json_entry)
+
+
+    # for entry in entries:
+    idd = 13
+    title = 'My party'
+    start = '2015-11-12T09:20:22+00:00'
+    end = '2015-11-12T13:20:22+00:00'
+
+    json_entry_p = {'id':idd, 'start':start, 'end':end, 'title': title}
+
+    # print json_entry
+
+    json_list.append(json_entry_p)
+    
+
+    # for entry in entries:
+    idd = 14
+    title = 'My big event'
+    start = '2015-11-17T09:20:22+00:00'
+    end = '2015-11-17T13:20:22+00:00'
+
+    json_entry_d = {'id':idd, 'start':start, 'end':end, 'title': title}
+
+    # print json_entry
+
+    json_list.append(json_entry_d)
+
+    
+
+    # # Open a file for writing
+    # out_file = open("js/test.json","w")
+
+    # # Save the dictionary into this file
+    # # (the 'indent=4' is optional, but makes it more readable)
+    # json.dump(json_list,out_file, indent=4)                                    
+
+    # # Close the file
+    # out_file.close()
+
+
+    return json.dumps(json_list)
+
+
+JSON = """[{"start": "2015-11-05T09:20:22+00:00", "end": "2015-11-05T13:20:22+00:00", "id": 12, "title": "My first event"}, {"start": "2015-11-12T09:20:22+00:00", "end": "2015-11-12T13:20:22+00:00", "id": 13, "title": "My party"}, {"start": "2015-11-17T09:20:22+00:00", "end": "2015-11-17T13:20:22+00:00", "id": 14, "title": "My big event"}]"""
 
 class Feed(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello')
+        self.response.write(jsonfeed())
 
+
+# ///////////////////////////////////
 
 
 # We set a parent key on the 'Greetings' to ensure that they are all
