d3e52b9a644e10048c04c3d8cd2a58fdc4f9baa2
ammaartawil <ammaar@ug-vpn58.ecs.soton.ac.uk>
2015-11-12 14:18:28 +0000

    Deleted shit

--- index.py
+++ index.py
@@ -161,15 +161,6 @@ TEST_HTML = """<html class="no-js" lang="">
                                     <div style="width:100%" class="input-group">
                                       <input type="text" name="name" placeholder="Event Name" class="form-control" aria-label="...">
                                       <div style="width:100%" class="btn-group btn-input clearfix">
-                                          <!--<button type="button" class="btn btn-default dropdown-toggle form-control" data-toggle="dropdown">
-                                              <span data-bind="label">Type of Event</span>&nbsp;<span class="caret"></span>
-                                          </button>
-                                          <ul style="width:100%" class="dropdown-menu" role="menu">
-                                              <li><a href="#">Module</a></li>
-                                              <li><a href="#">Society</a></li>
-                                              <li><a href="#">Job</a></li>
-                                              <li><a href="#">Other</a></li>
-                                          </ul>-->
                                           <select name="event_type" style="width:100%">
                                               <option value="module">Module</option>
                                               <option value="society">Society</option>
