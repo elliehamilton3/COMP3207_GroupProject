a53484560a56e4075b546316845fa79da8e379b5
Clayton Jones <clayton.jones93@googlemail.com>
2015-11-12 17:29:58 +0000

    Pulls data from database
    Still need to get right format etc

--- index.py
+++ index.py
@@ -477,46 +477,29 @@ def jsonfeed(startDate, endDate):
 
     json_list = []
 
-    # for entry in entries:
-    idd = 12
-    title = 'My event'
-    start = '2015-11-05T09:20:22+00:00'
-    end = '2015-11-05T13:20:22+00:00'
+    # # Query interface constructs a query using instance methods
+    q = Event.all()
+    # # q.filter("last_name =", "Smith")
+    # # q.filter("height <=", max_height)
+    # # q.order("-height")
 
-    json_entry = {'id':idd, 'start':start, 'end':end, 'title': title}
+    # # Query is not executed until results are accessed
+    for p in q.run(limit=5):
 
-    # print json_entry
+        # for entry in entries:
+        title = p.name
+        start_time = p.start_time
+        end_time = p.end_time
 
-    json_list.append(json_entry)
+        json_entry = {'start':start_time, 'title': title, 'end': end_time}
 
+        # print json_entry
 
-    # for entry in entries:
-    idd = 13
-    title = 'My party'
-    start = '2015-11-12T09:20:22+00:00'
-    end = '2015-11-12T13:20:22+00:00'
+        json_list.append(json_entry)
 
-    json_entry_p = {'id':idd, 'start':start, 'end':end, 'title': title}
-
-    # print json_entry
-
-    json_list.append(json_entry_p)
-    
-
-    # for entry in entries:
-    idd = 14
-    title = 'My big event'
-    start = '2015-11-17T09:20:22+00:00'
-    end = '2015-11-17T13:20:22+00:00'
-
-    json_entry_d = {'id':idd, 'start':start, 'end':end, 'title': title}
-
-    # print json_entry
-
-    json_list.append(json_entry_d)
-
-
+    # return json_list
     return json.dumps(json_list)
+
 
 
 class Feed(webapp2.RequestHandler):
